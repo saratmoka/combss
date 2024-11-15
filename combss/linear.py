59831a1f2aae3bfa15f73cebef8d9dccb8678588
h-hu2 <yanggghu@gmail.com>
2024-11-15 15:48:19 +1100

    Final package

--- combss/linear.py
+++ combss/linear.py
@@ -7,7 +7,7 @@ subset selection via continuous optimisation, as proposed in the original paper 
 All optimisation logic is handled in the private _optimisation.py module.
 """
 
-import combss.optimisation as optimisation
+import combsstest.optimisation as optimisation
 
 
 class model:
